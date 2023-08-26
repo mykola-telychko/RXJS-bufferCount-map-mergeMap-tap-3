e00dd439f6edb75749c643734af5ad9aa3def22f
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-26 23:47:00 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { fromEvent, of } from 'rxjs';
 import { bufferCount, map, mergeMap, tap } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/buffercount
+// Example 3: Last n keyboard presses tracking
 const fakeKeyPressesPost = (input$) =>
   of(201).pipe(
     tap((_) => {
